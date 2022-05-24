247ea36eb190a69654490654fcecb8a4270e492c
Changes729 <changes729@163.com>
2022-05-24 10:09:43 +0800

    fix bug cannot run.

--- Assets/Controller/TouchPartController.cs
+++ Assets/Controller/TouchPartController.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class TouchPartController : MonoBehaviour
 {
-    public void Init(ActionItemData data)
-    {
-
-    }
-
-
     // Start is called before the first frame update
     void Start()
     {
